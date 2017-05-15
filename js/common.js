66204447977492fcf0454206b5d6a0585faf5ba8
duriskz <korebinikovdmitry@gmail.com>
2017-05-15 12:14:35 +0600

    Пп
    ппп

---
+++ js/common.js
@@ -0,0 +1,43 @@
+$(function() {
+	$('.popup-modal').magnificPopup();
+	$(document).on('click', '.popup-modal-dismiss', function (e) {
+		e.preventDefault();
+		$.magnificPopup.close();
+	});
+	var $tab_item = $('.tab.active').data('tab-id');
+	if($('.tab.active')){
+		$('#load_more').attr('data-tab-id', $tab_item);
+		$('.tab_item[data-tab-id="' + $tab_item + '"] .item_content:hidden').slice(0, 3).addClass('show');
+	}
+
+	$('#load_more[data-tab-id="' + $tab_item + '"]').click(function(e) {
+		e.preventDefault();
+		$('.tab_item.active .item_content:hidden').slice(0, 3).addClass('show');
+	});
+	$('.tab').click(function(){
+		var $tab_item = $(this).data('tab-id');
+		if($(this).hasClass('active')){
+		}else{
+			$('.tab').removeClass('active');
+			$(this).addClass('active');
+			$('.tab_item').removeClass('active');
+			$('.tab_item[data-tab-id="' + $tab_item + '"]').addClass('active');
+			$('.tab_item.active .item_content:hidden').slice(0, 3).addClass('show');
+		}
+	});
+
+
+	$('a[href*="#"]:not([href="#"])').not('.popup-modal').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+					$('html, body').animate({
+							scrollTop: target.offset().top
+					}, 1000);
+				}
+				return false;
+		}
+	});
+	$(".phone").mask("+7 (999) 999 99 99");
+});
